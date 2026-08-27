08c4e4bd4aa7dcddb0fb0d24272264cfcbeff55d
matheusworkmonteiro <matheusworkmonteiro@gmail.com>
2026-08-26 23:33:31 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker instalado!');
-});
-
-self.addEventListener('fetch', (event) => {
-  // Aqui configuraremos o funcionamento offline no futuro
-});
